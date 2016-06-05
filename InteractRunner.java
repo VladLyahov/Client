f54b84ed09653ef1fe0576a4280232fe1d4fbaea
adnimd <adnimd@gmail.com>
2016-06-05 14:01:25 +0300

    Added work with the result, minor improvements, add commit

--- InteractRunner.java
+++ InteractRunner.java
@@ -6,36 +6,58 @@ import java.util.Scanner;
 public class InteractRunner {
 	
 	public static void main(String[] args) throws MyException{
+		/*
+			Сканер для считывания вводимого текста (открываем поток ввода)
+		*/
 		Scanner reader = new Scanner(System.in);
 		try{
 			Calculator calc = new Calculator();
 			String exit = "no";
+			String saveResult = "yes";
+			/*
+				Цикл проверяющий выходить из калькулятора или нет
+			*/
 			while(!exit.equals("yes")){
-				System.out.println("Enter first arg : ");
-				String first = reader.next();
-				System.out.println("Enter operation \"+\", \"-\", \"/\", \"*\" : ");
-				String action = reader.next();
-				System.out.println("Enter second arg : ");
-				String second = reader.next();
-				if(action.equals("+")){
-						calc.plus(Double.valueOf(first), Double.valueOf(second));
+					System.out.println("Enter first arg : ");
+					Double first = Double.valueOf(reader.next());
+				/*
+					Цикл отвечает за работу с результатом
+				*/
+				while(!saveResult.equals("no")){
+					System.out.println("Enter operation \"+\", \"-\", \"/\", \"*\" : ");
+					String action = reader.next();
+					System.out.println("Enter second arg : ");
+					Double second = Double.valueOf(reader.next());
+					if(action.equals("+")){
+							calc.plus(first, second);
+					}
+					else if(action.equals("-")){
+							calc.minus(first, second);
+					}
+					else if(action.equals("/")){
+							calc.div(first, second);
+					}
+					else if(action.equals("*")){
+							calc.multiply(first, second);
+					}
+					System.out.println("Result : " + calc.getResult());
+					System.out.println("Continue operation : yes/no ");
+					saveResult = reader.next();
+					/*
+						Если продолжаем работу с результатом, то записываем 
+						его в первое число и сразу выбираем действие
+					*/
+					if(!saveResult.equals("no")){
+						first = calc.getResult();
+						calc.cleanResult();
+					}
 				}
-				if(action.equals("-")){
-						calc.minus(Double.valueOf(first), Double.valueOf(second));
-				}
-				if(action.equals("/")){
-						calc.div(Double.valueOf(first), Double.valueOf(second));
-				}
-				if(action.equals("*")){
-						calc.multiply(Double.valueOf(first), Double.valueOf(second));
-				}
-				System.out.println("Result : " + calc.getResult());
 				calc.cleanResult();
 				System.out.println("Exit : yes/no ");
 				exit = reader.next();
 			}
 		} finally {
-		reader.close();
+		reader.close(); // закрываем поток ввода
 		}
 	}
 }
