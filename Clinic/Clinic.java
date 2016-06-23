cddc934274da60d8a60e13216333514bd10700cd
adnimd <adnimd@gmail.com>
2016-06-23 13:35:48 +0300

    remove Animal and CatDog; finish version

--- Clinic/Clinic.java
+++ Clinic/Clinic.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
 	Класс описывает клинику
@@ -11,9 +12,10 @@ public class Clinic{
 	*/
 
 	private final ArrayList<Client> clients;
+	Scanner scanner = new Scanner(System.in);
 
-	public Clinic(final int sizeClinic){
-		this.clients = new ArrayList<>(sizeClinic);
+	public Clinic(){
+		this.clients = new ArrayList<>();
 	}
 	/**
 	Добавить клиента
@@ -21,58 +23,125 @@ public class Clinic{
 	@param client
 	*/
 
-	public void addClient(final int position, final Client client){
-		// занятоста ли ячейка
-		if(this.client.get(position) == null){
+	public void addClient(final Client client){
+
 			// есть ли такой клиент
+		if(clients.size() > 0){
+			boolean isTrue = true;
 			for(Client cloneClient : clients){
-				if(cloneClient.equals(client)){
-					System.out.println("Клиент уже существует " + cloneClient.getName() + " " + cloneClient.getPet());
-				}
-				else{
-					this.clients.set(position, client);
+				if(cloneClient.getName().equals(client.getName())){
+					System.out.println("Клиент " + cloneClient.getName() + " " + " уже существует ");
+					isTrue = false;
 				}
 			}
+			if(isTrue == true){
+				this.clients.add(client);
+				System.out.println("Клиент add");
+			}
+			
 		}
 		else{
-			System.out.println("Клиент уже записан в эту ячейку");
+			this.clients.add(client);
+			System.out.println("Клиент add");
 		}
+
 	}
 	/**
 	Поиск клиента по имени его питомца
 	@param name имя питомца
 	*/
 	public Client findClientsByPetName(final String name){
-		Client findClient;
-		for(int i = 0; i < clients.size(); i++){
-			
-			String namePet = clients.get(i).getPet().getName();
-			if(namePet.equals(name)){
-				findClient = clients.get(i);
+		Client findClient = null;
+		if(clients.size() > 0){
+			for(int i = 0; i < clients.size(); i++){
+				
+				String namePet = clients.get(i).getPet().getName();
+				if(namePet.equals(name)){
+					findClient = clients.get(i);
+				}
 			}
 		}
-		System.out.println(findClientPet);
+		
 		return findClient;
 	}
 	/**
 	Поиск клиента по имени
 	@param name имя клиента
 	*/
+	public Client findClientsByName(final String name){
+		Client findClient = null;
+		if(clients.size() > 0){
+			for(int i = 0; i < clients.size(); i++){
+				
+				String nameClient = clients.get(i).getName();
+				if(nameClient.equals(name)){
+					findClient = clients.get(i);
+				}
+			}
+		}
+		return findClient;
+	}
 
 	/**
 	Редактирование имени клиента
 	@param name имя клиента
 	*/
+	public void renameClientName(final String name){
+			for(int i = 0; i < clients.size(); i++){
+				
+				String nameClient = clients.get(i).getName();
+				if(nameClient.equals(name)){
+					System.out.println("Add new name:");
+					String newName = scanner.nextLine();
+					Pet pet = clients.get(i).getPet();
+					clients.remove(i);
+					clients.set(i, new Client(newName, pet));
+				}
+			}
 
+	}
 	/**
 	Редактирование имени питомца
 	@param name имя питомца
 	*/
+	public void renamePetName(final String nameClient){
+			for(int i = 0; i < clients.size(); i++){
+				
+				String findNameClient = clients.get(i).getName();
+				if(nameClient.equals(findNameClient)){
+					System.out.println("Add new pet name:");
+					String newPetName = scanner.nextLine();
+					if(clients.get(i).getPet() instanceof Cat){
+						clients.remove(i);
+						clients.set(i, new Client(findNameClient, new Cat(newPetName)));
+					}
+					else if(clients.get(i).getPet() instanceof Dog){
+						clients.remove(i);
+						clients.set(i, new Client(findNameClient, new Dog(newPetName)));
+					}
+				}
+			}
 
+	}
 	/**
 	Удаление клиента
 	@param name имя питомца
 	*/
+	public void removeClient(final String name){
 
+		if(clients.size() > 0){
+			for(int i = 0; i < clients.size(); i++){
+				
+				String nameClient = clients.get(i).getName();
+				if(nameClient.equals(name)){
+					clients.remove(i);
+				}
+			}
+		}
+		else{
+			System.out.println("Клиника не содержит клиентов");
+		}
+	
+	}
 
 }
--- Clinic/ClinicRunner.java
+++ Clinic/ClinicRunner.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 
 /**
 	Работа клиники
@@ -8,14 +8,22 @@ import java.util.Scanner;
 public class ClinicRunner{
 
 	public static void main(String[] args) {
-		final Clinic clinic = new Clinic(10);
-		Scanner scanner = new Scanner(System.in);
-		String command = scanner.nextLine();
+		final Clinic clinic = new Clinic();
 
-		clinic.addClient(0, new Client("Jonny", new Cat("Klaus")));
-		clinic.addClient(1, new Client("Mary", new Dog("Pshok")));
-		clinic.findClientsByPetName("Pshok");
-
+		clinic.addClient(new Client("Jonny", new Cat("Klaus")));
+		clinic.addClient(new Client("Mary", new Dog("Pshok")));
+		clinic.addClient(new Client("Fedor", new Cat("Luise")));
+		clinic.addClient(new Client("Mary", new Dog("Pshochik")));
+		//System.out.println(clinic.findClientsByPetName("Pshok").getName());
+		//System.out.println(clinic.findClientsByName("Jonny").getName());
+		//clinic.renameClientName("Jonny");
+		//System.out.println("Name rename" );
+		//System.out.println(clinic.findClientsByPetName("Klaus").getName());
+		System.out.println(clinic.findClientsByName("Mary").getPet().getName());
+		clinic.renamePetName("Mary");
+		System.out.println(clinic.findClientsByName("Mary").getPet().getName());
+		clinic.removeClient("Mary");
+		//System.out.println(clinic.findClientsByName("Mary").getName());
 	}
 	
 }
